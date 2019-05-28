d6bb76b3725cba59a5a6f0e28e9fdc1a15e53f7a
Mathias <mathias.de.weerdt@gmail.com>
2019-05-28 10:57:50 +0200

    Changed build script and added more configs.

--- build.sh
+++ build.sh
@@ -1,22 +1,19 @@
-# !bin/bash
-shouldBuild=false
+#!/bin/bash
+shouldBuild=0
 
-if [ $1 == "--run" ]
+if [ $1 == "--help" ]
 then
-    shouldBuild=true
-fi
-
-if [ $1 == "--build" ]
-then
-    shouldBuild=false
+    echo "Usage: ./build.sh --[run|build|switch] --[local|staging|production]"
+    exit 1
 fi
 
 if [ $2 == "--local" ]
 then
+    cp android/app/google-services-local.json android/app/google-services.json
+
     if grep -q "org.jimber.threebotlogin.staging" "android/app/build.gradle";
     then
         sed -i -e 's/org.jimber.threebotlogin.staging/org.jimber.threebotlogin/g' android/app/build.gradle
-        sed -i -e 's/org.jimber.threebotlogin.staging/org.jimber.threebotlogin/g' android/app/google-services.json
         sed -i -e 's/org.jimber.threebotlogin.staging/org.jimber.threebotlogin/g' android/app/src/debug/AndroidManifest.xml
         sed -i -e 's/org.jimber.threebotlogin.staging/org.jimber.threebotlogin/g' android/app/src/main/AndroidManifest.xml
         sed -i -e 's/android:label="3bot_staging"/android:label="3bot"/g' android/app/src/main/AndroidManifest.xml   
@@ -28,7 +25,6 @@ then
     if ! grep -q "org.jimber.threebotlogin.local" "android/app/build.gradle";
     then
         sed -i -e 's/org.jimber.threebotlogin/org.jimber.threebotlogin.local/g' android/app/build.gradle
-        sed -i -e 's/org.jimber.threebotlogin/org.jimber.threebotlogin.local/g' android/app/google-services.json
         sed -i -e 's/org.jimber.threebotlogin/org.jimber.threebotlogin.local/g' android/app/src/debug/AndroidManifest.xml
         sed -i -e 's/org.jimber.threebotlogin/org.jimber.threebotlogin.local/g' android/app/src/main/AndroidManifest.xml
         sed -i -e 's/android:label="3bot"/android:label="3bot_local"/g' android/app/src/main/AndroidManifest.xml
@@ -37,20 +33,28 @@ then
         sed -i -e 's/org.jimber.threebotlogin/org.jimber.threebotlogin.local/g' ios/Runner/Info.plist
     fi
 
-    if [ $shouldBuild == true ]
+    if [ $1 == "--run" ]
     then
+        echo "flutter run -t lib/main_local.dart"
         flutter run -t lib/main_local.dart
+    elif [ $1 == "--switch" ]
+    then
+        echo "Switched configs local."
     else
+        echo "flutter build apk -t lib/main_local.dart"
         flutter build apk -t lib/main_local.dart
     fi
+
+    exit 1
 fi
 
 if [ $2 == "--staging" ]
 then
+    cp android/app/google-services-staging.json android/app/google-services.json
+
     if grep -q "org.jimber.threebotlogin.local" "android/app/build.gradle";
     then
         sed -i -e 's/org.jimber.threebotlogin.local/org.jimber.threebotlogin/g' android/app/build.gradle
-        sed -i -e 's/org.jimber.threebotlogin.local/org.jimber.threebotlogin/g' android/app/google-services.json
         sed -i -e 's/org.jimber.threebotlogin.local/org.jimber.threebotlogin/g' android/app/src/debug/AndroidManifest.xml
         sed -i -e 's/org.jimber.threebotlogin.local/org.jimber.threebotlogin/g' android/app/src/main/AndroidManifest.xml
         sed -i -e 's/android:label="3bot_local"/android:label="3bot"/g' android/app/src/main/AndroidManifest.xml
@@ -62,7 +66,6 @@ then
     if ! grep -q "org.jimber.threebotlogin.staging" "android/app/build.gradle";
     then
         sed -i -e 's/org.jimber.threebotlogin/org.jimber.threebotlogin.staging/g' android/app/build.gradle
-        sed -i -e 's/org.jimber.threebotlogin/org.jimber.threebotlogin.staging/g' android/app/google-services.json
         sed -i -e 's/org.jimber.threebotlogin/org.jimber.threebotlogin.staging/g' android/app/src/debug/AndroidManifest.xml
         sed -i -e 's/org.jimber.threebotlogin/org.jimber.threebotlogin.staging/g' android/app/src/main/AndroidManifest.xml
         sed -i -e 's/android:label="3bot"/android:label="3bot_staging"/g' android/app/src/main/AndroidManifest.xml
@@ -71,20 +74,28 @@ then
         sed -i -e 's/org.jimber.threebotlogin/org.jimber.threebotlogin.staging/g' ios/Runner/Info.plist
     fi
 
-    if [ $shouldBuild == true ]
+    if [ $1 == "--run" ]
     then
+        echo "flutter run -t lib/main_staging.dart"
         flutter run -t lib/main_staging.dart
+    elif [ $1 == "--switch" ]
+    then
+        echo "Switched configs staging."
     else
+        echo "flutter build apk -t lib/main_staging.dart"
         flutter build apk -t lib/main_staging.dart
     fi
+
+    exit 1
 fi
 
 if [ $2 == "--production" ]
 then
+    cp android/app/google-services-prod.json android/app/google-services.json
+
     if grep -q "org.jimber.threebotlogin.local" "android/app/build.gradle";
     then
         sed -i -e 's/org.jimber.threebotlogin.local/org.jimber.threebotlogin/g' android/app/build.gradle
-        sed -i -e 's/org.jimber.threebotlogin.local/org.jimber.threebotlogin/g' android/app/google-services.json
         sed -i -e 's/org.jimber.threebotlogin.local/org.jimber.threebotlogin/g' android/app/src/debug/AndroidManifest.xml
         sed -i -e 's/org.jimber.threebotlogin.local/org.jimber.threebotlogin/g' android/app/src/main/AndroidManifest.xml
         sed -i -e 's/android:label="3bot_local"/android:label="3bot"/g' android/app/src/main/AndroidManifest.xml
@@ -96,7 +107,6 @@ then
     if grep -q "org.jimber.threebotlogin.staging" "android/app/build.gradle";
     then
         sed -i -e 's/org.jimber.threebotlogin.staging/org.jimber.threebotlogin/g' android/app/build.gradle
-        sed -i -e 's/org.jimber.threebotlogin.staging/org.jimber.threebotlogin/g' android/app/google-services.json
         sed -i -e 's/org.jimber.threebotlogin.staging/org.jimber.threebotlogin/g' android/app/src/debug/AndroidManifest.xml
         sed -i -e 's/org.jimber.threebotlogin.staging/org.jimber.threebotlogin/g' android/app/src/main/AndroidManifest.xml
         sed -i -e 's/android:label="3bot_staging"/android:label="3bot"/g' android/app/src/main/AndroidManifest.xml
@@ -105,10 +115,21 @@ then
         sed -i -e 's/org.jimber.threebotlogin.staging/org.jimber.threebotlogin/g' ios/Runner/Info.plist
     fi
 
-    if [ $shouldBuild == true ]
+    if [ $1 == "--run" ]
     then
+        echo "flutter run -t lib/main_prod.dart"
         flutter run -t lib/main_prod.dart
+    elif [ $1 == "--switch" ]
+    then
+        echo "Switched configs to production."
     else
+        echo "flutter build apk -t lib/main_prod.dart"
         flutter build apk -t lib/main_prod.dart
     fi
+
+    exit 1
 fi
+
+echo "Syntax error."
+echo "Usage: ./build.sh --[run|build|switch] --[local|staging|production]"
+exit 1

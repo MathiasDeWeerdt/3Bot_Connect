7f576c78ed4d3249b289efe5a2b9fe55eccd2ab5
coeneivan <ivan@ivancoene.be>
2019-08-08 17:02:32 +0200

    Formating

--- lib/screens/SuccessfulScreen.dart
+++ lib/screens/SuccessfulScreen.dart
@@ -10,19 +10,19 @@ class SuccessfulScreen extends StatefulWidget {
 }
 
 Future<bool> _onWillPop() {
-    var index = 0;
-    
-    for (var flutterWebViewPlugin in flutterWebViewPlugins) {
-      if (flutterWebViewPlugin != null) {
-        if (index == lastAppUsed) {
-          flutterWebViewPlugin.show(); 
-          showButton = true;
-        }
-        index++;
+  var index = 0;
+
+  for (var flutterWebViewPlugin in flutterWebViewPlugins) {
+    if (flutterWebViewPlugin != null) {
+      if (index == lastAppUsed) {
+        flutterWebViewPlugin.show();
+        showButton = true;
       }
+      index++;
     }
-    return Future.value(true);
   }
+  return Future.value(true);
+}
 
 class _SuccessfulScreenState extends State<SuccessfulScreen> {
   @override
@@ -30,41 +30,47 @@ class _SuccessfulScreenState extends State<SuccessfulScreen> {
     return WillPopScope(
       onWillPop: _onWillPop,
       child: Scaffold(
-          appBar: AppBar(
-            title: Text('Logged in'),
-            elevation: 0.0,
-          ),
-          body: Container(
-              width: double.infinity,
-              height: double.infinity,
-              color: Theme.of(context).primaryColor,
+        appBar: AppBar(
+          title: Text('Logged in'),
+          elevation: 0.0,
+        ),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Theme.of(context).primaryColor,
+          child: Container(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0))),
               child: Container(
-                  child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(20.0),
-                              topRight: Radius.circular(20.0))),
-                      child: Container(
-                          padding: EdgeInsets.only(top: 24.0, bottom: 38.0),
-                          child: Center(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                Icon(
-                                  Icons.check_circle,
-                                  size: 42.0,
-                                  color: Theme.of(context).accentColor,
-                                ),
-                                SizedBox(
-                                  height: 20.0,
-                                ),
-                                Text('You are logged in, go back to the browser now.'),
-                                SizedBox(
-                                  height: 60.0,
-                                ),
-                              ],
-                            ),
-                        )))))));
+                padding: EdgeInsets.only(top: 24.0, bottom: 38.0),
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Icon(
+                        Icons.check_circle,
+                        size: 42.0,
+                        color: Theme.of(context).accentColor,
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Text('You are logged in, go back to the browser now.'),
+                      SizedBox(
+                        height: 60.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }

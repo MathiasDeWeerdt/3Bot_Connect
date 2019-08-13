632b016811f3091d87836c7d63dd456e5fb30b2c
jasper <jasper.baes@gmail.com>
2019-08-13 12:02:36 +0200

    add switch buttons

--- lib/widgets/scopeDialog.dart
+++ lib/widgets/scopeDialog.dart
@@ -3,6 +3,13 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:threebotlogin/widgets/CustomDialog.dart';
 
+
+
+void changeValue(value) {
+  print('TAPPED: ${value}');
+  value = !value;
+}
+
 showScopeDialog(context, Map<dynamic, dynamic> scope, String appId, callback, {cancelCallback}) {
   // flutter defined function
   print('scope is    ' + jsonEncode(scope));
@@ -43,33 +50,16 @@ Widget scopeList(context, Map<dynamic, dynamic> scope) {
           val = 'Cryptographic key pair';
         }
         return Container(
-          margin: EdgeInsets.only(bottom: 20.0),
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(right: 10),
-                child: Icon(
-                  Icons.keyboard_arrow_right,
-                  size: 32.0,
-                  color: Colors.black,
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    keys[index]?.toUpperCase(),
-                    textAlign: TextAlign.left,
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    val,
-                    textAlign: TextAlign.left,
-                  )
-                ],
-              )
-            ],
-          ),
+          child: SwitchListTile(
+            value: true,
+            activeColor: Theme.of(context).primaryColor,
+            onChanged: (bool val) => changeValue(val),
+            title: Text( 
+              keys[index]?.toUpperCase(), 
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(val),
+            secondary: const Icon(Icons.keyboard_arrow_right, size: 32,),),
         );
       });
 }

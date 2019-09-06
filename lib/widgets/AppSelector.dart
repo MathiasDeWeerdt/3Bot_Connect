46bc065b666307e24939275a2c761fe284c0c483
Mathias <mathias.de.weerdt@gmail.com>
2019-09-06 14:46:13 +0200

    Added app info and derivedSeed logic

--- lib/widgets/AppSelector.dart
+++ lib/widgets/AppSelector.dart
@@ -108,11 +108,14 @@ class _AppSelectorState extends State<AppSelector> {
         final state = randomString(15);
 
         final privateKey = await getPrivateKey();
-        final signedHash = signData(state, privateKey);
+        final signedHash = await signData(state, privateKey);
 
         var jsToExecute =
             "(function() { try {window.localStorage.setItem('tempKeys', \'{\"privateKey\": \"${keys["privateKey"]}\", \"publicKey\": \"${keys["publicKey"]}\"}\');  window.localStorage.setItem('state', '$state'); } catch (err) { return err; } })();";
+        
+        // This should be removed in the future!
         sleep(const Duration(seconds: 1));
+
         final res =
             await flutterWebViewPlugins[appId].evalJavascript(jsToExecute);
         final appid = apps[appId]['appid'];
@@ -126,9 +129,10 @@ class _AppSelectorState extends State<AppSelector> {
         var jsonData = jsonEncode(encrypted);
         var data = Uri.encodeQueryComponent(jsonData); //Uri.encodeFull();
 
-        loadUrl =
-            'https://$appid$redirecturl${union}username=${await getDoubleName()}&signedhash=${Uri.encodeQueryComponent(await signedHash)}&data=$data';
+        loadUrl = 'http://$appid$redirecturl${union}username=${await getDoubleName()}&signedhash=${Uri.encodeQueryComponent(signedHash)}&data=$data';
 
+
+        logger.log("!!!loadUrl: " + loadUrl);
         flutterWebViewPlugins[appId].reloadUrl(loadUrl);
         print("Eval result: $res");
 
